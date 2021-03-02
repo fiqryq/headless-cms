d7420a80c36ab141e6d017df588689800d1c0de3
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-03-02 20:21:01 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c0f7b470c80c4bd62078aade7e006b46'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '53fc47e3-e94f-4667-810c-a27e0178d542'
+};
